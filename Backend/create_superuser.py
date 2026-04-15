ac651898859c9b4e03bff3b284699fdd2ebf6859
bruno8899 <sahgautam791@gmail.com>
2026-04-15 09:50:09 +0545

    Fix superuser script for custom user model

--- Backend/create_superuser.py
+++ Backend/create_superuser.py
@@ -7,8 +7,8 @@ django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'gautamsah4271@gmail.com', 'Admin@123')
+if not User.objects.filter(email='gautamsah4271@gmail.com').exists():
+    User.objects.create_superuser(email='gautamsah4271@gmail.com', password='Admin@123')
     print('Superuser created!')
 else:
     print('Superuser already exists.')
