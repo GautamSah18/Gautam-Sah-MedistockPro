d981935d2db0fafd03ebd1cb818d10f46e9b87fb
bruno8899 <sahgautam791@gmail.com>
2026-05-02 20:16:49 +0545

    Fix Redis crash issue

--- Backend/MedistockPro/settings.py
+++ Backend/MedistockPro/settings.py
@@ -141,16 +141,22 @@ WSGI_APPLICATION = 'MedistockPro.wsgi.application'
 
 ASGI_APPLICATION = "MedistockPro.asgi.application"
 
-REDIS_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379")
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [REDIS_URL],
+if REDIS_URL:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.core.RedisChannelLayer",
+            "CONFIG": {
+                "hosts": [REDIS_URL],
+            },
         },
-    },
-}
+    }
+else:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels.layers.InMemoryChannelLayer",
+        },
+    }
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
